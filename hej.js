841c7cfe4b60c3755c680889f8a539510d8dbb71
BoTolv <47107472+BoTolv@users.noreply.github.com>
2019-05-28 17:06:39 +0200

    igen med onload

--- hej.js
+++ hej.js
@@ -4,6 +4,6 @@ function openBrowser(url){
 	var options = "location = yes"
 	var ref = cordova.InAppBrowser.open(url, target, options);
 }
-function onLoad(){
+function Hej(){
 	window.open('https://affald.000webhostapp.com/index.html');
 }
