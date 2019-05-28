c0423356f74a0c29cdf9199dea94fdf39f09cf20
BoTolv <47107472+BoTolv@users.noreply.github.com>
2019-05-28 13:12:24 +0200

    notifikation halløj

--- hej.js
+++ hej.js
@@ -4,17 +4,3 @@ function openBrowser(url){
 	var options = "location = yes"
 	var ref = cordova.InAppBrowser.open(url, target, options);
 }
-if (“Notification” in window) {
-  Notification.requestPermission(function (permission) {
-    
-    if (permission === ‘granted’) {
-      var notification = new Notification(“Første notifikation”, {
-           tag: ‘message1’, 
-           body: “My body” 
-      }); 
-      notification.onshow  = function() { console.log(‘show’); };
-      notification.onclose = function() { console.log(‘close’); };
-      notification.onclick = function() { console.log(‘click’); };
-    }
-  });
-}
