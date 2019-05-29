8ba1a97b71a71675df9af9f1090c725aaba85a0d
BoTolv <47107472+BoTolv@users.noreply.github.com>
2019-05-29 13:05:45 +0200

    notifikationer igen

--- hej.js
+++ hej.js
@@ -4,11 +4,7 @@ function openBrowser(url){
 	var options = "location = yes"
 	var ref = cordova.InAppBrowser.open(url, target, options);
 }
-function App(){
-	window.open('https://affald.000webhostapp.com/index.html');
-}
 function onDeviceReady(){
-	app.receivedEvent('deviceready');
-	if(PushbotsPlugin.isAndroid){
-		PushbotsPlugin.initializeAndroid("5ced1fe70540a303df34e263", "119545800858")
+	onload('https://affald.000webhostapp.com/index.html');
 }
+
